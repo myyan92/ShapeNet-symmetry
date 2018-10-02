3bc8b437d2ff7162b7d2b6c2b7b1f34ada2edb5f
Anastasia Dubrovina <anastasia@Anastasias-MacBook-Pro.local>
2018-10-01 23:24:24 -0700

    corrected bugs

--- symmetry_detection/Main_ShapeNetParts.jl
+++ symmetry_detection/Main_ShapeNetParts.jl
@@ -19,12 +19,12 @@ const path2results = "/orions4-zfs/projects/anastasiad/ShapeNet-symmetry/Results
         end
 	    println(filename)
 	    newMesh = loadMesh_v2(filename)
-	    logname = "/orions4-zfs/projects/anastasiad/ShapeNet-symmetry/Results/03001627/" * modelname * "_$partnum.log"
+	    logname = "/orions4-zfs/projects/anastasiad/ShapeNet-symmetry/Results/03001627_v2/" * modelname * "_$partnum.log"
 	    println(logname)
 	    fout = open(logname, "w")
 	    symType, canonical, translate = detectSelfSymmetry(newMesh, fout)
 	    close(fout)
-	    symname = "/orions4-zfs/projects/anastasiad/ShapeNet-symmetry/Results/03001627/" * modelname * "_$partnum.sym"
+	    symname = "/orions4-zfs/projects/anastasiad/ShapeNet-symmetry/Results/03001627_v2/" * modelname * "_$partnum.sym"
 	    println(symname)
 	    saveSymmetry(symname, symType, translate, canonical)
     end
