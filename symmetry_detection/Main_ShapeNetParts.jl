db8a251cc1900b128cfb98db0d69752863bfaacb
Anastasia Dubrovina <12185704+anastasiadk@users.noreply.github.com>
2018-10-23 21:10:12 -0700

    updated paths + models from in "./all_model_lists/<synsetID>.txt"

--- symmetry_detection/Main_ShapeNetParts.jl
+++ symmetry_detection/Main_ShapeNetParts.jl
@@ -8,11 +8,13 @@ using FileIO
 using IOUtil
 using SymmetryFeatureEmbeddingLib
 
-const path2obj = "/orions3-zfs/projects/anastasiad/ShapeNetSymm/Minhyuk-style-OBJ/"
+const path2obj = "/orions4-zfs/projects/jyau/SymmDetProject/testing/Minhyuk-style-OBJ/"
+# const path2results = "/orions4-zfs/projects/anastasiad/ShapeNet-symmetry/Results/03001627/"
+# const path2obj = "/orions3-zfs/projects/anastasiad/ShapeNetSymm/Minhyuk-style-OBJ/"
 const path2results = "/orions3-zfs/projects/anastasiad/ShapeNetSymm/Results/03001627/"
 
 @everywhere function main(path2obj, modelname)
-	for partnum = 0:50
+	for partnum = 0:1000
 		filename = "/orions4-zfs/projects/jyau/SymmDetProject/testing/Minhyuk-style-OBJ/" * modelname * "/" * "$partnum.obj"
         if (!isfile(filename))
         	break
@@ -33,11 +35,10 @@ end
 # synsetID = ARGS[1]
 synsetID = "03001627"
 println(synsetID)
-models = readall("./deduplicate_lists/" * synsetID * ".txt")
+# models = readall("./deduplicate_lists/" * synsetID * ".txt")
+models = readall("./all_model_lists/" * synsetID * ".txt")
 models = split(models, '\n')
-#path2obj = fill(path2obj, size(models,1))
 println(size(models,1))
-# pmap(main, path2obj, models)
 for model_id = 1:size(models,1)
 	main(path2obj[1],models[model_id])
 end
